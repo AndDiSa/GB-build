2d459df2bbc7073b36b0609a5b642e989909a37d
Dirk Sagwitz <dsagwitz@web.de>
2011-02-20 12:56:11 +0100

    extract data package while installing

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -142,6 +142,7 @@ class EdifyGenerator(object):
       self.mounts.add(p.mount_point)
     else:
       what = mount_point.lstrip("/")
+      if what == "userdata" : mount_point = "/data"
       what = self.info.get("partition_path", "") + what
       self.script.append('mount("%s", "%s", "%s", "%s");' %
                          (self.info["fs_type"], self.info["partition_type"],
